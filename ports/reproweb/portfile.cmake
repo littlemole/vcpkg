68e7125ca62b7ab10ca3b2d9cf01dcabe1b718d8
mike <mike@oha7.org>
2018-05-18 21:30:47 +0200

    add reproweb port

---
+++ ports/reproweb/portfile.cmake
@@ -0,0 +1,36 @@
+# Common Ambient Variables:
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
+#
+
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/repro-web-0.0.1)
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/littlemole/repro-web/archive/0.0.1.tar.gz"
+    FILENAME "reproweb-0.0.1.tgz"
+    SHA512 4d2afcb8b81d9be9cee63887f39bea70e53306dd464c67cfb976283d4337376d43b60093ce122568f6975a9ddee6906ce2e8bdfa62ac23d83f17f7df26de9d16
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
+)
+
+vcpkg_install_cmake()
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/reproweb RENAME copyright)
+
+
+ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
