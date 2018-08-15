e684fc076cc6d3f9023a173e4b94faa9989a0aea
littlemole <little.mole@oha7.org>
2018-08-16 00:07:50 +0200

    update reprocpp to 0.0.12

--- ports/reprocpp/portfile.cmake
+++ ports/reprocpp/portfile.cmake
@@ -11,11 +11,11 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/repro-0.0.11)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/repro-0.0.12)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/littlemole/repro/archive/0.0.11.tar.gz"
-    FILENAME "repro-0.0.11.tgz"
-    SHA512 88214cf47c51f2fadf7953c46f3aca843b9d84f925413f189b5040be931b2bfa43b0824b9bcb08c2d22b64cf3273aca91faa675cbe489e9c942fced30f9d34bc
+    URLS "https://github.com/littlemole/repro/archive/0.0.12.tar.gz"
+    FILENAME "repro-0.0.12.tgz"
+    SHA512 e1ff0fd4710014da5c3a9ef217f2d44b147d04236aba8aa022dffd310b9be25cec66a1980587efacc3eabc4d9d4ae99fe6353ddb5ef8798ada9e2e6aa38b3f8b
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
