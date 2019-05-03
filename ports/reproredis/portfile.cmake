61de69a4d81ae41c73501448572b1f6a44f613ad
littlemole <little.mole@oha7.org>
2019-05-03 15:29:42 +0200

    update reproredis to 0.0.16

--- ports/reproredis/portfile.cmake
+++ ports/reproredis/portfile.cmake
@@ -11,11 +11,11 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/repro-redis-0.0.15)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/repro-redis-0.0.16)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/littlemole/repro-redis/archive/0.0.15.tar.gz"
-    FILENAME "reproredis-0.0.15.tgz"
-    SHA512 6df64d75cf9911c95395798efda6516b7ba25d63a026eb64611229aac5fa403786efd6c1bf8d8fbd81673b6f7687f288c230775246aa0b0cc3461a319d99efab
+    URLS "https://github.com/littlemole/repro-redis/archive/0.0.16.tar.gz"
+    FILENAME "reproredis-0.0.16.tgz"
+    SHA512 c156b90ff5a929ff0b98ba7b19e4640b2fc0ed4ade66b8d74ab0b01e54863f9cca31fb568758a750b36a9be3a2fb1648015af7ac6d1f7c43c79a8ec10a164fbf
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
