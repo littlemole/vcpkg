c4e78e1571a4ecad9943e00f9b93ca53a7e47215
littlemole <little.mole@oha7.org>
2019-08-04 12:11:39 +0200

    update priocpp to 0.0.43

--- ports/priocpp/portfile.cmake
+++ ports/priocpp/portfile.cmake
@@ -11,11 +11,11 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/prio-0.0.42)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/prio-0.0.43)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/littlemole/prio/archive/0.0.42.tar.gz"
-    FILENAME "prio-0.0.42.tgz"
-    SHA512 dcc078b50e6dc6049597deef6286f3ce2f4c5e3cf27bc7f536fcb2b13b014340058f827f5ff9bccfc5124f2d14edd1c3b606df42abbab748b234a012458f6099
+    URLS "https://github.com/littlemole/prio/archive/0.0.43.tar.gz"
+    FILENAME "prio-0.0.43.tgz"
+    SHA512 87eb814c86b98bb6fac9a0e0c6640ccf7ba90d4c9a800b2ab579a9b519dc0677e93d6c9650d143c70567b359521a8d8cf9b1538ffab8738c6315b7ce0b9a7140
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
