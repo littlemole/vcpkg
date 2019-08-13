0478e8e4605c3d405dee408aa715bf82cc437a1f
littlemole <little.mole@oha7.org>
2019-08-14 00:07:41 +0200

    update cryptoneat to 0.0.13

--- ports/cryptoneat/portfile.cmake
+++ ports/cryptoneat/portfile.cmake
@@ -11,11 +11,11 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cryptoneat-0.0.12)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cryptoneat-0.0.13)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/littlemole/cryptoneat/archive/0.0.12.tar.gz"
-    FILENAME "cryptoneat-0.0.12.tgz"
-    SHA512 dce6e5c850e544a76b44a06beccb0f0922a7e1e9b49fbe1af3c87a1036517bb102c8844be9eb334c2c0553f2fb13bec44bb786b7317efe8b22eea5d1a22bd927
+    URLS "https://github.com/littlemole/cryptoneat/archive/0.0.13.tar.gz"
+    FILENAME "cryptoneat-0.0.13.tgz"
+    SHA512 d067f2dd4b680b11218e69c0e3323fbe17f317649268b67bd07734bb240a4aceeac8f2c388558ca3de979649fe0f3a5f4dea8a54edaecf1bdacd49bb639ae02f
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
