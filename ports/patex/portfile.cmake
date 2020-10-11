453bc2d954134d28f9e5cae70a52a22361f56a01
mike <mike@oha7.org>
2020-10-11 22:48:43 +0200

    patex

---
+++ ports/patex/portfile.cmake
@@ -0,0 +1,36 @@
+# Common Ambient Variables:
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
+#
+
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/patex-0.0.1)
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/littlemole/patex/archive/0.0.1.tar.gz"
+    FILENAME "patex-0.0.1.tgz"
+    SHA512 70a711d7e0d1def1cb542174f4f521e3823e236e77c16196bed4b18aec0006f194b4f68ff7af43647604861372229a18e52574ddd32ccf3acc85d2e7d0092482
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
+)
+
+vcpkg_install_cmake()
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/reprocurl RENAME copyright)
+
+
+ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
